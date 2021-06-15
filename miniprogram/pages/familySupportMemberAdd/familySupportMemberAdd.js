c4aa5d736f21d87122f6c9dd80acffd19aec9ebd
HQ-Dev <hq_dev@163.com>
2021-06-15 14:41:53 +0800

    入驻申请更新

--- miniprogram/pages/familySupportMemberAdd/familySupportMemberAdd.js
+++ miniprogram/pages/familySupportMemberAdd/familySupportMemberAdd.js
@@ -20,7 +20,8 @@ Page({
     furnitureRepair:false,  // 家具维装
     rushPipe:false,  // 管道疏通
     others:false,  // 其他
-    pics:[]  //
+    pics:[],  // 其它个人信息图片
+    otherInfoFileIDs:new Array() // 其它个人信息图片 fileID 集合
   },
 
   /**
@@ -187,12 +188,12 @@ Page({
    * 照片增加绑定
    */
   imageAdd: function(param) {
+    console.log(param.detail)
     this.setData({
       pics:param.detail.all
     })
-    console.log(param.detail)
   },
-  /**
+  /** 
    * 照片去除绑定
    */
   imageRemove: function(param) {
@@ -202,13 +203,27 @@ Page({
   },
 
   submitUserInfo: async function(param) {
+    // this.printBindingInfos();
+    // 上传身份证图片，获取 fileID
+    await this.storeIDCardFilesIntoCloudDB();
 
-    this.printBindingInfos();
-    // 先上传图片，获取 fileID，文件夹名称
+    // 上传个人信息图片，获取 fileID
+    await this.storeManInfoFilesIntoCloudDB();
+    
+    // 提交审核存储申请信息到与数据库
+    this.storeInfoIntoCloudDB();
+
+    console.log("上传完成，更新完成")
+  },
+
+  /**
+   * 将需要审核的身份证图片存到云存储，获得图片的 fileID 并绑定
+   */
+  storeIDCardFilesIntoCloudDB: async function() {
     // const cloudPath = `pics/个人入驻界面/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}` + this.data.idcardUrlFront.match(/\.[^.]+?$/);
     let idcardUrlArray = [this.data.idcardUrlFront[0], this.data.idcardUrlBack[0]];
     for (let i = 0; i < idcardUrlArray.length; i++) {
-      const cloudPath = `pics/个人入驻界面/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}`;
+      const cloudPath = `pics/个人入驻界面/身份证/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}`;
       await wx.cloud.uploadFile({
         cloudPath,
         filePath:idcardUrlArray[i],
@@ -226,7 +241,33 @@ Page({
         console.log(error)
       })
     }
+  },
+  /**
+   * 将需要审核的个人信息图片存到云存储，获得图片的 fileID 并绑定
+   */
+  storeManInfoFilesIntoCloudDB: async function() {
+    // const cloudPath = `pics/个人入驻界面/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}` + this.data.idcardUrlFront.match(/\.[^.]+?$/);
+    let picsArray = new Array();
+    for (let i = 0; i < this.data.pics.length; i++) {
+      const cloudPath = `pics/个人入驻界面/其它信息/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}`;
+      await wx.cloud.uploadFile({
+        cloudPath,
+        filePath:this.data.pics[i].url,
+      }).then(res=> {
+        picsArray.push(res.fileID)
+        this.setData({
+          otherInfoFileIDs:picsArray
+        })
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
 
+  /**
+   * 将入驻申请的信息（个人信息、照片的 fileID）存入到数据库
+   */
+  storeInfoIntoCloudDB: function() {
     // 将图片在云存储中的相关信息与服务提供者的其他信息一同添加到数据库集合中 (serverProviderInfo)
     const db = wx.cloud.database();
     const _ = db.command;
@@ -235,14 +276,18 @@ Page({
         trueName:this.data.trueName,
         idCardNo:this.data.idCardNumber,
         phoneNumber:this.data.phoneNumber,
-        folders:
-          {folderName:"个人入驻界面",
+        folders:[
+          {folderName:"个人入驻界面/身份证",
             files:
               {
                 idCardFrontFileID:this.data.idcardFrontFileID,
                 idCardBackFileID:this.data.idcardBackFileID
               }
           },
+          {folderName:"个人入驻界面/其它信息",
+            files:this.data.otherInfoFileIDs
+          },
+        ],
         selfIntroduction:this.data.selfIntroduction,
         serverFields:
           {
@@ -257,15 +302,8 @@ Page({
     }).then(res => {
       console.log(res)
     }).catch(error => {
-
     })
   },
-
-
-  /**
-   * 上传
-   */
-
 
   printBindingInfos: function() {
     console.log(this.data.pics);
